e48ff3ca6974d15c41b0683152b5c15763c978c8
mn1210 <xuzhen1210@aliyun.com>
2019-04-01 10:39:30 +0800

    项目聚合搭建

---
+++ springboot-multi-demo-web/src/test/java/org/springboot/multi/demo/web/SpringbootMultiDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.springboot.multi.demo.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootMultiDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
